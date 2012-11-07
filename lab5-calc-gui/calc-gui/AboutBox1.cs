bf92f457cfe0bb005cb96645ce2001a1bdd243e3
Alex Egg <eggie5@gmail.com>
2012-11-06 18:28:32 -0800

    removed extra sln file and added close about form button

--- lab5-calc-gui/calc-gui/AboutBox1.cs
+++ lab5-calc-gui/calc-gui/AboutBox1.cs
@@ -101,6 +101,11 @@ namespace calc_gui
         }
         #endregion
 
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
  
     }
 }
